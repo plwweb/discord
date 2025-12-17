cf4320830d8710938e4a47a6cc362a175b2078f3
plwweb <pracha.phuluangwit@gmail.com>
2025-12-18 02:48:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client } = require('discord.js-selfbot-v13');
 const client = new Client({ checkUpdate: false });
 
 client.on('ready', async () => {
-  console.log(`✅ บัญชี ${client.user.tag} ออนไลน์พร้อมสถานะ`);
+  console.log(`✅ บัญชี ${client.user.tag} ออนไลน์พร้อมสถานะ Lastt`);
 
   // ตั้งค่า Presence
   client.user.setPresence({
@@ -15,18 +15,15 @@ client.on('ready', async () => {
         state: '24/7', // แก้ไขข้อความที่นี่
         emoji: { name: '🔥' } // แก้ไข Emoji ที่นี่
       },
-      {
-        // 2. ส่วนของเกม ROBLOX (แบบมีโลโก้)
-        name: "ROBLOX",
-        type: 'PLAYING',
-        applicationId: '439205569915518976',
-        assets: {
-          largeImage: '439205569915518976', // ID รูปมาตรฐานของ Roblox
-          largeText: 'Roblox'
-        },
-        timestamps: {
-            start: Date.now()
-        }
+      
+      // สร้าง Object สำหรับ Roblox Rich Presence
+  const rblx = new RichPresence(client)
+    .setApplicationId('439205569915518976') // ID ของ Roblox
+    .setType('PLAYING')
+    .setName('Roblox')
+    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1423715705882280056/1450936935227985940/image.png?ex=694459d2&is=69430852&hm=beeea752e66e1726b5c6b336e34e8058e85792e282b93d6e0ebc711f4d221666') // 🔴 แก้ไข: ใส่เป็น Link รูปภาพ
+    .setAssetsLargeText('Roblox')
+    .setStartTimestamp(Date.now());
       }
     ]
   });
