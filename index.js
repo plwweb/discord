f157af3518a113bd4feab046f1612f925df6d87b
plwweb <pracha.phuluangwit@gmail.com>
2025-12-18 18:43:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const client = new Client({ checkUpdate: false });
 
 client.on('ready', async () => {
   console.log(`✅ ${client.user.tag} ออนไลน์ต่อเนื่องแบบ No-Downtime!`);
-
-client.on('ready', async () => {
   console.log(`✅ บัญชี ${client.user.tag} ออนไลน์พร้อมสถานะ Roblox!`);
 
   // 1. ส่วนของ Roblox Rich Presence
@@ -12,13 +10,13 @@ client.on('ready', async () => {
     .setApplicationId('439205569915518976')
     .setType('PLAYING')
     .setName('Roblox')
-    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1423715705882280056/1450936935227985940/image.png?ex=694459d2&is=69430852&hm=beeea752e66e1726b5c6b336e34e8058e85792e282b93d6e0ebc711f4d221666') // ใช้ URL รูป
+    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1423715705882280056/1450936935227985940/image.png?ex=694459d2&is=69430852&hm=beeea752e66e1726b5c6b336e34e8058e85792e282b93d6e0ebc711f4d221666')
     .setAssetsLargeText('Roblox')
     .setStartTimestamp(Date.now());
 
   // 2. ส่วนของ Custom Status
   const customStatus = {
-    name: 'Custom Status', // 🔴 บรรทัดนี้สำคัญมาก ต้องใส่กัน error
+    name: 'Custom Status', 
     type: 'CUSTOM',
     state: '24/7 nakub', 
     emoji: { name: '🔥' } 
